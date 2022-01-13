6092d2bcc76ae7ebc46cc38275bd888b82b0f3e0
lizlove <elizabethlovero@gmail.com>
2022-01-13 09:17:40 -0500

    Add scrolly.

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,6 +10,7 @@
     $header = $("#header"),
     $footer = $("#footer"),
     $main = $("#main"),
+    $nav = $('#nav'),
     settings = {
       // Parallax background effect?
       parallax: true,
@@ -85,7 +86,70 @@
     });
   }
 
-  // Main Sections: Links.
+  // Nav.
+  var $nav_a = $nav.find('a');
+
+  $nav_a
+    .addClass('scrolly')
+    .on('click', function() {
+
+      var $this = $(this);
+
+      // External link? Bail.
+        if ($this.attr('href').charAt(0) != '#')
+          return;
+
+      // Deactivate all links.
+        $nav_a.removeClass('active');
+
+      // Activate link *and* lock it (so Scrollex doesn't try to activate other links as we're scrolling to this one's section).
+        $this
+          .addClass('active')
+          .addClass('active-locked');
+
+    })
+    .each(function() {
+
+      var	$this = $(this),
+        id = $this.attr('href'),
+        $section = $(id);
+
+      // No section for this link? Bail.
+        if ($section.length < 1)
+          return;
+
+      // Scrollex.
+        $section.scrollex({
+          mode: 'middle',
+          top: '5vh',
+          bottom: '5vh',
+          initialize: function() {
+
+            // Deactivate section.
+              $section.addClass('inactive');
+
+          },
+          enter: function() {
+
+            // Activate section.
+              $section.removeClass('inactive');
+
+            // No locked links? Deactivate all links and activate this section's one.
+              if ($nav_a.filter('.active-locked').length == 0) {
+
+                $nav_a.removeClass('active');
+                $this.addClass('active');
+
+              }
+
+            // Otherwise, if this section's link is the one that's locked, unlock it.
+              else if ($this.hasClass('active-locked'))
+                $this.removeClass('active-locked');
+
+          }
+        });
+
+    });
 
 
 })(jQuery);
